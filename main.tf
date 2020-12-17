53f97c20a679e307bce29e724116f8ef142b3f54
Robin Norwood <rnorwood@hashicorp.com>
2020-12-17 16:58:27 -0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_string" "lb_id" {
+  length  = 3
+  special = false
+}
+
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "2.4.0"
+
+  # Ensure load balancer name is unique
+  name = "lb-${random_string.lb_id.result}-tutorial-example"
+
+  internal = false
+
+  security_groups = []
+  subnets         = []
+
+  number_of_instances = length(aws_instance.app)
+  instances           = aws_instance.app.*.id
+
+  listener = [{
+    instance_port     = "80"
+    instance_protocol = "HTTP"
+    lb_port           = "80"
+    lb_protocol       = "HTTP"
+  }]
+
+  health_check = {
+    target              = "HTTP:80/index.html"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+  }
+}
+
+resource "aws_instance" "app" {
+  ami = "ami-04d29b6f966df1537"
+
+  instance_type = var.instance_type
+
+  subnet_id              = ""
+  vpc_security_group_ids = []
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
+}
